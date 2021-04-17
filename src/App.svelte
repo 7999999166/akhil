778aaab1a1a01cce50155576dd0a9752b3d6f5b2
Kanik Goyal <acrotechllp@gmail.com>
2021-04-18 00:47:09 +0530

    second commit

--- src/App.svelte
+++ src/App.svelte
@@ -5,10 +5,60 @@
     filename: 'AppData.db',
     autoload: true
   });
-</script>
+  import halfmoon from 'halfmoon';
+  import './halfmoon.min.css';
+  import './w3.css';
+import { create_component } from 'svelte/internal';
+  halfmoon.onDOMContentLoaded();
 
-  <nav>
-    <Link to="/">Home</Link>
-    <Link to="/products">Products</Link>
-  </nav>
+  var cc = {'color' : 'blue' }
+
+  function mouseover(event){
+
+  }
+</script>
+<style>
+  .main{
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+    height: 100%;
+
+  }
+  .nav{
+    height: 12vh;
+    background-color: black;
+    display: flex;
+    justify-content: space-evenly;
+    color: cornsilk;
+    align-items: center;
+  }
+  .aa{
+    font-size: larger;
+  }
+
+  /* .bb:hover{
+  color: rgb(0, 255, 136);
+   text-decoration: none !important;
+  
+  } */
+
+  a:hover{
+    text-decoration: none;
+  }
+
+
+
+
+</style>
+<div class="main">
+<div class='nav w3-top'>
+<h1 class="aa"><a href="/">Home</a></h1>
+<h1 class="aa"><a href="/products">Products</a></h1>
+<h1 class="aa"><a href="/">Home</a></h1>
+<h1 class="aa"><a href="/">Home</a></h1>
+<h1 class="aa"><a href="/">Home</a></h1>
+<h1 class="aa"><a href="/">Home</a></h1>
+</div> 
   <RouterView />
+  </div>
--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,1 +1,7 @@
+<style>
+    h1{
+        margin-top: 100px;
+    }
+</style>
 <h1>Hey I M The Best</h1>
+
